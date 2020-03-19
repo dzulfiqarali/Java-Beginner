ebb89e64d70214e6b363eca1c2d8715866f988e7
unknown <dzulfiqar05@gmail.com>
2020-03-19 19:36:07 +0700

    Belajar Method

---
+++ src/com/FirstProject/Main.java
@@ -0,0 +1,13 @@
+package com.FirstProject;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        selamatPagi("Jul");
+    }
+
+    public static void selamatPagi(String nama){
+        System.out.println("Selamat pagi " + nama);
+    }
+}
