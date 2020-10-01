a3091ca98ba43d654b521d749661285feb40d6ca
Addy-codes <72205091+Addy-codes@users.noreply.github.com>
2020-10-01 08:53:21 -0700

    Update Main.java

--- src/com/FirstProject/Main.java
+++ src/com/FirstProject/Main.java
@@ -1,13 +1,13 @@
 package com.FirstProject;
 
-public class Main {
+public class Main { // defining a class named as 'Main'. Note: the class name cannot be main or any token 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //defining the main function
 	// write your code here
-        selamatPagi("Jul");
+        selamatPagi("Jul");// calling another function and sending a String parameter "Jul"
     }
 
-    public static void selamatPagi(String nama){
-        System.out.println("Selamat pagi " + nama);
+    public static void selamatPagi(String nama){//defining another function
+        System.out.println("Selamat pagi " + nama);//printing a statement
     }
 }
